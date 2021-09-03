780e734e6ecce4dec2d403851a8c4f1a151757b4
Chamindu Sauranga <chamindu1567@gmail.com>
2021-09-03 22:44:38 +0530

    Completed the test problem for 'Max2'

--- src/test/scala/problems/Max2Tests.scala
+++ src/test/scala/problems/Max2Tests.scala
@@ -4,18 +4,20 @@ package problems
 import chisel3.iotesters.PeekPokeTester
 
 // Problem:
-//
+//Problem has been Completed
+
 // Implement test with PeekPokeTester
 //
 class Max2Tests(c: Max2) extends PeekPokeTester(c) {
   for (i <- 0 until 10) {
 
     // Implement below ----------
-
-    poke(c.io.in0, 0)
-    poke(c.io.in1, 0)
+    val in0 = rnd.nextInt(256)
+    val in1 = rnd.nextInt(256)
+    poke(c.io.in0, in0)
+    poke(c.io.in1, in1)
     step(1)
-    expect(c.io.out, 1)
+    expect(c.io.out, if (in0>in1) in0 else in1)
 
     // Implement above ----------
   }
