99bfbaf8b15d08a649302ce134bca7137196e94e
Chamindu Sauranga <chamindu1567@gmail.com>
2021-11-23 22:22:15 +0530

    Completed VectorShiftRegister

--- src/main/scala/problems/VecShiftRegister.scala
+++ src/main/scala/problems/VecShiftRegister.scala
@@ -17,9 +17,17 @@ class VecShiftRegister extends Module {
     val shift = Input(Bool())
     val out   = Output(UInt(4.W))
   })
-  // Implement below ----------
-
-  io.out := 0.U
-
-  // Implement above ----------
+  val delays = Reg(Vec(4, UInt()))
+  when (io.load) {
+    delays(0) := io.ins(0)
+    delays(1) := io.ins(1)
+    delays(2) := io.ins(2)
+    delays(3) := io.ins(3)
+  } .elsewhen(io.shift) {
+    delays(0) := io.ins(0)
+    delays(1) := delays(0)
+    delays(2) := delays(1)
+    delays(3) := delays(2)
+  }
+  io.out := delays(3)
 }
